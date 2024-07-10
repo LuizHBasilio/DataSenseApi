818f8386047cef94f2879cfacfa588f50dd359ae
Luiz <luizbasilio@fitec.org.br>
2024-07-10 13:23:39 -0300

    Added get method

--- src/main/java/com/DataSense/DataSenseApi/controller/EquipamentDataController.java
+++ src/main/java/com/DataSense/DataSenseApi/controller/EquipamentDataController.java
@@ -1,6 +1,12 @@
 package com.DataSense.DataSenseApi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,23 @@ public class EquipamentDataController {
 
 	@Autowired
     private EquipamentDataRepository equipamentDataRepository;
+	
+	@GetMapping
+    public List<EquipmentData> getAllEquipmentData() {
+        return equipamentDataRepository.findAll();
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<?> getEquipmentDataById(@PathVariable Long id) {
+		
+        EquipmentData equipmentData = equipamentDataRepository.findById(id).orElse(null);
+        
+        if (equipmentData != null) {
+            return ResponseEntity.ok(equipmentData);
+        } else {
+        	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Equipment with id " + id + " was not found");
+        }
+    }
 
     @PostMapping
     public EquipmentData receiveEquipamentData(@RequestBody EquipmentData equipmentData) {
