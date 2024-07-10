97275a949c282d27f4a91dd730a7b103b8df906a
zuiioll <luizbasilio@fitec.org.br>
2024-07-10 08:22:11 -0300

    Initial commit

---
+++ src/test/java/com/DataSense/DataSenseApi/DataSenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataSense.DataSenseApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
