13506f0c06b8e216a74fc29a0c0d0aebd3d3f01a
Luiz <luizbasilio@fitec.org.br>
2024-07-17 12:14:38 -0300

    Updates on front end

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,35 +1,41 @@
 let myChart;
 
-async function fetchAverage() {
-    const equipmentId = document.getElementById('equipmentId').value;
-    const period = document.getElementById('period').value;
+async function getAverage() {
+    const equipmentId = document.getElementById("equipmentId").value;
+    const period = document.getElementById("period").value;
+
     const response = await fetch(`/datasense/api/v1/equipments/average?period=${period}&equipmentId=${equipmentId}`);
     const data = await response.json();
 
-    const ctx = document.getElementById('myChart').getContext('2d');
+    const ctx = document.getElementById("myChart").getContext("2d");
 
+    // Verifica se o gráfico já foi criado
     if (myChart) {
-        myChart.destroy();
-    }
-
-    myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: [`Average for ${period}`],
-            datasets: [{
-                label: 'Average Value',
-                data: [data.average],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        // Atualiza os dados e redesenha o gráfico
+        myChart.data.datasets[0].data = [data.average];
+        myChart.data.datasets[0].label = `Average for ${equipmentId} over ${period}`;
+        myChart.update();
+    } else {
+        // Cria um novo gráfico
+        myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Average Value'],
+                datasets: [{
+                    label: `Average for ${equipmentId} over ${period}`,
+                    data: [data.average],
+                    backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+                    borderColor: ['rgba(75, 192, 192, 1)'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
